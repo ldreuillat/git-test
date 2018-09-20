a95150e6dbaa0147a8e80c2a4a8828068221e11e
Laurent DREUILLAT <ldreuillat@ausy.fr>
2018-09-20 16:28:10 +0200

    Test Feature 2

--- src/main.java
+++ src/main.java
@@ -1,6 +1,7 @@
 public class main {
 	public static void main(String[] args) {
 		System.out.println("Application test GIT");
-		System.out.println("TESTING Feature");
+		System.out.println("TESTING Feature 1");
+		System.out.println("TESTING Feature 2");
 	}
 }
