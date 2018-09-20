dfa30c36c19a9a6c817d6b9b885fe92fdf9b7a67
Laurent DREUILLAT <ldreuillat@ausy.fr>
2018-09-20 16:07:06 +0200

    Display version

--- src/main.java
+++ src/main.java
@@ -1,5 +1,8 @@
 public class main {
+	private static final String VERSION="1.1";
+	
 	public static void main(String[] args) {
-		System.out?println("Application test GIT");
+		System.out.println("Application test GIT");
+		System.out.println("Version : " + VERSION);
 	}
 }
